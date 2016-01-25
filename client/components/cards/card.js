28c14a1c068d5885ca077f923aa642398654326f
Fred Chapman <areinei123@gmail.com>
2016-01-25 17:13:09 -0500

    card editing

--- client/components/cards/card.js
+++ client/components/cards/card.js
@@ -1,8 +1,7 @@
 Template.cards.helpers ({
   cards: function(){
-    // var setId = Sets.findOne(Session.get('setId'));
-    // console.log(setId);
-    return Cards.find({})//({setId: setId._id});
+    var setId = Session.get('setId');
+    return Cards.find({setId: setId});
   }
 });
 
@@ -14,46 +13,49 @@ Template.cards.helpers ({
 
 Template.cardNew.helpers({
   setData: function() {
-    return Sets.findOne(Session.get('setId'));
+    return Session.get('setId');
+  },
+  editCard: function() {
+    return Template.instance().editCard.get();
   }
 });
 
 Template.cardNew.events({
   "keyup #cardName": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-name").html(line);
+    $("#card-name").html(line);
   },
   "keyup #manaCost": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-mana-cost").html(line);
+    $("#card-mana-cost").html(line);
   },
   "keyup #mainType": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-main-type").html(line);
+    $("#card-main-type").html(line);
   },
   "keyup #subType": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-sub-type").html(line);
+    $("#card-sub-type").html(line);
   },
   "keyup #rulesText": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-rules-text").html(line);
+    $("#card-rules-text").html(line);
   },
   "keyup #flavorText": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-flavor-text").html(line);
+    $("#card-flavor-text").html(line);
   },
   "keyup #power": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-power").html(line);
+    $("#card-power").html(line);
   },
   "keyup #toughness": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-toughness").html(line);
+    $("#card-toughness").html(line);
   },
   "keyup #imageUrl": function(event) {
     var line = String($(event.currentTarget).val());
-    $(".card-image").css('background-image','url('+line+')');
+    $("#card-image").css('background-image','url('+line+')');
   },
   "submit .new-card": function(event) {
     event.preventDefault();
@@ -68,7 +70,7 @@ Template.cardNew.events({
     // var file = $('#imageUpload').get(0).files[0];
     // var fileObj = Images.insert(file);
     var imageUrl = event.target.imageUrl.value;
-    var setId = Sets.findOne(Session.get('setId'));
+    var setId = Session.get('setId');
     Cards.insert({
       name: name,
       manaCost: manaCost,
@@ -90,5 +92,91 @@ Template.cardNew.events({
     event.target.power.value = "";
     event.target.toughness.value = "";
     event.target.imageUrl.value = "";
+  }
+});
+
+Template.card.helpers({
+  editCard: function(){
+    return Template.instance().editCard.get();
+  }
+})
+
+Template.card.events({
+  "click .delete": function(){
+    event.preventDefault();
+    Cards.remove(this._id);
+  },
+  'click .edit': function(event, template) {
+    event.preventDefault();
+    var editCard = template.editCard.get();
+    template.editCard.set(!editCard);
+  }
+});
+
+Template.card.created = function(){
+  this.editCard = new ReactiveVar(false);
+};
+
+Template.cardEdit.events({
+  "keyup #cardName": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-name").html(line);
+  },
+  "keyup #manaCost": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-mana-cost").html(line);
+  },
+  "keyup #mainType": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-main-type").html(line);
+  },
+  "keyup #subType": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-sub-type").html(line);
+  },
+  "keyup #rulesText": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-rules-text").html(line);
+  },
+  "keyup #flavorText": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-flavor-text").html(line);
+  },
+  "keyup #power": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-power").html(line);
+  },
+  "keyup #toughness": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-toughness").html(line);
+  },
+  "keyup #imageUrl": function(event) {
+    var line = String($(event.currentTarget).val());
+    $("#card-image").css('background-image','url('+line+')');
+  },
+  "submit .new-card": function(event) {
+    event.preventDefault();
+    var name = event.target.cardName.value;
+    var manaCost = event.target.manaCost.value;
+    var mainType = event.target.mainType.value;
+    var subType = event.target.subType.value;
+    var rulesText = event.target.rulesText.value;
+    var flavorText = event.target.flavorText.value;
+    var power = event.target.power.value;
+    var toughness = event.target.toughness.value;
+    var imageUrl = event.target.imageUrl.value;
+
+    Cards.update(this._id, { $set: {
+      name: name,
+      manaCost: manaCost,
+      mainType: mainType,
+      subType: subType,
+      rulesText: rulesText,
+      flavorText: flavorText,
+      power: power,
+      toughness: toughness,
+      imageUrl: imageUrl,
+    }
+    });
   }
 });
--- client/components/sets/set.js
+++ client/components/sets/set.js
@@ -22,26 +22,31 @@ Template.setIndex.events({
 Template.set.helpers({
   sets: function () {
     return Sets.find({});
-  }
-});
-
-Template.set.helpers({
+  },
   setData: function() {
     return Sets.findOne(Session.get('setId'));
   },
   newCard:function(){
     return Template.instance().newCard.get();
+  },
+  numberOfCards: function(){
+    var setId = Session.get('setId');
+    return Cards.find({setId: setId}).count();
   }
 });
 
 Template.set.events({
-  'click .card-new': function(event,template) {
-    event.preventDefault;
+  'click .card-new': function(event, template) {
+    event.preventDefault();
     var newCard = template.newCard.get();
     template.newCard.set(!newCard);
+  },
+  'click .print-layout': function(event){
+    event.preventDefault();
+    
   }
 });
 
-Template.set.created=function(){
-  this.newCard=new ReactiveVar(false);
+Template.set.created = function(){
+  this.newCard = new ReactiveVar(false);
 };
